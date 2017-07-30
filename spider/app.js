922d4cbc5a7231fa7fe0e386b2af6dd1a44de9df
leotangjc <tangjiachengemail@gmail.com>
2017-07-31 00:53:34 +0800

    爬虫练习

--- spider/app.js
+++ spider/app.js
@@ -25,17 +25,25 @@ app.get('/', function(req, res, next) {
     var items = [];
     $('.cell').each(function(idx, element) {
       var $element = $(element);
-      var a = $element.find('span.count_of_replies').text().replace(/[\r\n]/g, '');
+      var a = $element.find('span.count_of_replies').
+          text().
+          replace(/[\r\n]/g, '');
       if (Number(a) > 50) {
-        var b = $element.find('span.count_of_visits').text().replace(/[\r\n]/g, '');
+        var b = $element.find('span.count_of_visits').
+            text().
+            replace(/[\r\n]/g, '');
         if (Number(b) > 400) {
           items.push({
             href: $element.find('.topic_title').attr('href'),
             pic: $element.find('.last_time img').attr('src'),
             time: $element.find('.last_time span').text(),
             title: $element.find('.topic_title').attr('title'),
-            comment: $element.find('span.count_of_replies').text().replace(/[\r\n]/g, ''),
-            click: $element.find('span.count_of_visits').text().replace(/[\r\n]/g, '')
+            comment: $element.find('span.count_of_replies').
+                text().
+                replace(/[\r\n]/g, ''),
+            click: $element.find('span.count_of_visits').
+                text().
+                replace(/[\r\n]/g, ''),
           });
         } else {
           return null;
@@ -48,35 +56,44 @@ app.get('/', function(req, res, next) {
     //输出数组按点击数的升序排序
     // res.send(items.sort('click').reverse());
 
+
     //通过排序，然后通过输出第一个元素，来实现输出最小点击数的对象
     // var min = items.sort('click').reverse();
     // res.send(min[0]);
 
+    //-------------------
+
+    // 功能：找出数组中点击次数最多的元素对象
+    // 1: 遍历数组 2: 比较数组中每个元素的click 3：找出最大的click，记录它在数组中的位置
+    // 4: 首先我们要在数组遍历之外的作用域定义click,位置 5：比较数组中的click，如果比Min大，就将值赋予给他
 
     var i = 0;
     var min = null;
-    $("items.click").each(function(index, element){
-      // console.log(element);
-      if(min < index){
-        min = index[i];
-        i++;
+    items.forEach(function(element, index) {
+      // console.log(element, index)
+      console.log(min, element.click)
+      if (min < element.click) {
+        min = element.click;
+        i = index;
       }
     });
+    res.send(items[i]);
 
-    res.send(min);
-
-
-
-
-
-    // function maxx(items) {
-    //   var max = items[0];
-    //   for(var i = 1,ilen = items.length;i < ilen;i+=1){
-    //     if(max < items[1]){
-    //
-    //     }
-    //   }
+    // [{click:1},{click:2},{click:3}]
+    // obj
+    // {
+    //   name: "23",
+    //   age:22
     // }
+    // obj.name
+    // obj['name']
+    // obj[click]
+
+
+    //-------------------
+    //下面的东西没有学
+
+
 
 
   });
