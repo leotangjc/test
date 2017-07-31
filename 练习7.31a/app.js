cadc3c46508a7cf4b05a96c568ae2cbfac41b11f
leotangjc <tangjiachengemail@gmail.com>
2017-07-31 20:35:43 +0800

    求数组值的和，以及求出最大值和最小值

---
+++ 练习7.31a/app.js
@@ -0,0 +1,51 @@
+var express = require('express');
+
+var app = express();
+var superagent = require('superagent');
+//抓取
+var cheerio = require('cheerio');
+var url = require('url');
+
+var async = require('async');
+
+app.listen(3000, function(req, res) {
+  console.log('app is running at port 3000');
+});
+
+
+
+
+var a = [1, 33, 44, 22];
+
+// 最大值和最小值
+
+var min = null;
+a.forEach(function(element) {
+  //最大值
+  if (min < element) {
+  //最小值
+  // if (min > element) {
+    min = element;
+  }
+});
+console.log(min);
+
+
+
+//元素总和
+// var i = 0;
+// var min = null;
+// a.forEach(function(element, index) {
+//   // console.log(element);
+//   min = min + element;
+// });
+//
+// console.log(min)
+
+
+
+
+
+
+
+
