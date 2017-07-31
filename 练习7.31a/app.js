a5b1ad140d0a4321bc8ec031a242c7d33e7c203f
leotangjc <tangjiachengemail@gmail.com>
2017-07-31 23:49:09 +0800

    update

--- 练习7.31a/app.js
+++ 练习7.31a/app.js
@@ -1,10 +1,5 @@
 var express = require('express');
 var app = express();
-app.listen(3000, function(req, res) {
-  console.log('app is running at port 3000');
-});
-
-
 
 
 var a = [1, 33, 44, 22];
--- 练习7.31b/app.js
+++ 练习7.31b/app.js
@@ -1,39 +1,50 @@
 var express = require('express');
-
 var app = express();
-var superagent = require('superagent');
-//抓取
-var cheerio = require('cheerio');
-var url = require('url');
-
-var async = require('async');
-
-app.listen(3000, function(req, res) {
-  console.log('app is running at port 3000');
-});
-
-
-
 
 var a =  [{name:'jack',age:22},{name:'nancy',age:23},{name:'tom',age:22}];
 
-// console.log(a[1]);
-//1.比较出age值等于22的对象
-//2. 把拥有该值的对象添加进新数组，
-//3. 输出新数组
 
-// 最大值和最小值
-var h = new Array();
-var n = 22;
-var l = null;
-var i = 0;
-a.forEach(function(element, index) {
-  // console.log(element);
-  if (n == element.age) {
-    i = index;
-    h.unshift(a[i]);
-    // console.log(element['1']);
-  }
-});
-console.log(h);
 
+
+
+
+
+// //使用for循环数组，然后将符合要求的元素，添加到新的数组输出
+// var i;
+// var h = new Array();
+// for(i=0;i<a.length;i++){
+//   if(a[i].age === 22){
+//     h.unshift(a[i])
+//   }
+// }
+// console.log(h);
+
+
+
+// //使用filter筛选出值中带有22的对象
+// var b = a.filter(function(x) {
+//   return x.age === 22;
+// });
+//
+// console.log(b);
+
+
+
+
+
+// //1.比较出age值等于22的对象
+// //2. 把拥有该值的对象添加进新数组，
+// //3. 输出新数组
+//
+// var h = new Array();
+// var n = 22;
+// var l = null;
+// var i = 0;
+// a.forEach(function(element, index) {
+//   if (n == element.age) {
+//     i = index;
+//     h.unshift(a[i]);
+//   }
+// });
+// console.log(h);
+//
